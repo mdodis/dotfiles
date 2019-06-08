7e1a4449b9003eabf99cbab4b55fd6769eb981c1
mdodis <michaeldodisgr@gmail.com>
2019-06-08 16:41:45 +0300

    add capture mon

---
+++ scripts/select_monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#MON=$(xrandr -q | grep "^[a-Z]" | grep -i " connected")
+
+SEL=$(xrandr -q | grep "^[a-Z]" | grep -i " connected" | while read -r line; do
+    #echo "check: $line"
+    PROP=$(echo $line | awk '{print $3}')
+    NAME=$(echo $line | awk '{print $1}')
+
+    if [[ $PROP == "primary" ]]; then
+        PROP=$(echo $line | awk '{print $4}')
+    fi
+    echo "$NAME - $PROP"
+
+done | dmenu | sed 's/^.*- //')
+
+DIM=$(echo $SEL | sed 's/+.*$//')
+OFF=$(echo $SEL | sed 's/[0-9]*x[0-9]*//')
+
+echo "$DIM $OFF"

7fcfae74947ed2d99a03c5c9e520dac2b8954baf
mdodis <mdodis@ics.forth.gr>
2019-07-02 11:50:00 +0300

    added calc, some more book-keeping

--- scripts/select_monitor.sh
+++ scripts/select_monitor.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#MON=$(xrandr -q | grep "^[a-Z]" | grep -i " connected")
-
 SEL=$(xrandr -q | grep "^[a-Z]" | grep -i " connected" | while read -r line; do
     #echo "check: $line"
     PROP=$(echo $line | awk '{print $3}')
@@ -11,7 +9,7 @@ SEL=$(xrandr -q | grep "^[a-Z]" | grep -i " connected" | while read -r line; do
     fi
     echo "$NAME - $PROP"
 
-done | dmenu | sed 's/^.*- //')
+done | dmenu -p Monitor | sed 's/^.*- //')
 
 DIM=$(echo $SEL | sed 's/+.*$//')
 OFF=$(echo $SEL | sed 's/[0-9]*x[0-9]*//')
